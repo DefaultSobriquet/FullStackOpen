b904a1f1c7d4e3b4b9fb4a4fb047e29532e7c095
Default Sobriquet <defaultsobriquet@gmail.com>
2022-12-30 01:11:01 -0800

    Add Exercise 1.6 (unicafe step 1)

---
+++ part1/unicode/README.md
@@ -0,0 +1,3 @@
+# Full Stack Open - Exercises - Part 1d (unicode)
+
+This folder contains my submissions for Part 1d (unicode) of the [Full Stack Open 2022](https://fullstackopen.com/en/) course.
---
+++ part1/unicode/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+// Displays a title (header)
+const Title = ({text}) => <h1>{text}</h1>;
+
+// Button component
+const Button = ({onClick,text}) => (<button onClick={onClick}>{text}</button>);
+
+// Row of Buttons for Good / Neutral / Bad
+const ButtonRow = ({goodIncr, neutralIncr, badIncr}) => {
+  return (
+    <>
+      <Button onClick={goodIncr} text="Good"/>
+      <Button onClick={neutralIncr} text="Neutral"/>
+      <Button onClick={badIncr} text="Bad"/>
+    </>
+  )
+}
+
+// Stats component
+const Stats = ({name, value}) => (<p>{name} - {value}</p>);
+
+// Displays Stats for Good / Neutral / Bad
+const Display = ({goodNum, neutralNum, badNum}) => {
+  return (
+    <>
+      <Stats name="Good" value={goodNum} />
+      <Stats name="Neutral" value={neutralNum} />
+      <Stats name="Bad" value={badNum} />
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Title text="Give Feedback" />
+      <ButtonRow
+        goodIncr={() => setGood(good+1)}
+        neutralIncr={() => setNeutral(neutral+1)}
+        badIncr={() => setBad(bad+1)}
+      />
+      <Title text="Statistics" />
+      <Display goodNum={good} neutralNum={neutral} badNum={bad}/>
+    </div>
+  )
+}
+
+export default App
